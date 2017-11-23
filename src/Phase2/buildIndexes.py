098a2a25a371424c36f64e7856b4b0586abf1c75
Tymoorej <tymoore@ualberta.ca>
2017-11-22 20:07:54 -0700

    adding index files

--- src/Phase2/buildIndexes.py
+++ src/Phase2/buildIndexes.py
@@ -1,6 +1,8 @@
 from subprocess import call
+from subprocess import Popen
+from bsddb3 import db
 
-def main():
+def sort():
     t = "terms.txt"
     y = "years.txt"
     r = "recs.txt"
@@ -8,6 +10,25 @@ def main():
     call('sort -u -o {0} {1}'.format(t,t), shell=True)
     call('sort -u -o {0} {1}'.format(y,y), shell=True)
     call('sort -u -o {0} {1}'.format(r,r), shell=True)
+
+
+def create_indexies():
+    database=db.DB()
+    database.open("re.idx",None,db.DB_HASH,db.DB_CREATE)
+    database.close()
+
+    database=db.DB()
+    database.open("te.idx",None,db.DB_BTREE,db.DB_CREATE)
+    database.close()
+
+    database=db.DB()
+    database.open("ye.idx",None,db.DB_BTREE,db.DB_CREATE)
+    database.close()
+
+def main():
+    # sort()
+    # create_indexies()
+    Popen(["perl","break.pl"])
 
 
 if __name__ == '__main__':
