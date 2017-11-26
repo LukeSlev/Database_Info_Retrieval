f9f0c1498a006dd1ad57247362963e947321f18e
cmput274 <cmput274@ualberta.ca>
2017-11-25 17:49:38 -0700

    added meathod definitions

--- src/Phase 3/Queries.py
+++ src/Phase 3/Queries.py
@@ -1,5 +1,30 @@
 import bsddb3
 
+def parseQuery(query):
+    pass
+
+def yearGreaterThan():
+    pass
+
+def yearLessThan():
+    pass
+
+def yearEqualTo():
+    pass
+
+def titeEqualTo():
+    pass
+
+def authorEqualTo():
+    pass
+
+def otherEqualTo():
+    pass
+
+def joinQueries():
+    pass
+
+
 def main():
     pass
 
