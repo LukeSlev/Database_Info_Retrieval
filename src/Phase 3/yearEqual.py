27f158a0ba60a19390d9baa18153aae969ad6e5c
tymoorej <cmput274@ualberta.ca>
2017-11-26 16:48:52 -0700

    ?

--- src/Phase 3/yearEqual.py
+++ src/Phase 3/yearEqual.py
@@ -1,21 +1,14 @@
 from bsddb3 import db
 
-<<<<<<< HEAD
 def yearSearch(Starting_Year):
     Starting_Year=int(Starting_Year)-1
-=======
-def yearSearch(year):
->>>>>>> 0a844b256cad07faf31b6254283416e3444a280f
     DB_File = "ye.idx"
     database = db.DB()
     database.set_flags(db.DB_DUP) #declare duplicates allowed before you create the database
     database.open(DB_File,None, db.DB_BTREE, db.DB_CREATE)
     curs = database.cursor()
-    Starting_Year = int(year)-1
-    Ending_Year = int(year) + 1
-      
+
     middleSet = set()
-<<<<<<< HEAD
 
     #print(str(int(Starting_Year)))
 
@@ -26,32 +19,23 @@ def yearSearch(year):
     else:
         result = None
 
-=======
-    #get the record that has the smallest key greater than or equal to the Starting Name:
-    result = curs.set_range(str(Starting_Year).encode("utf-8")) 
-   
->>>>>>> 0a844b256cad07faf31b6254283416e3444a280f
     if(result != None):
-  
         while(result != None):
-            #Checking the end condition: If the student's name comes after(or equal to) Ending_Name
-            if(str(result[0].decode("utf-8")[0:len(str(Ending_Year))])>=str(Ending_Year)): 
-                break
-            
-         #   print("Year: " + str(result[0].decode("utf-8")) + ", Rest: " + str(result[1].decode("utf-8")))
-            middleSet.add(str(result[1].decode("utf-8")))
-            result = curs.next() 
+            #print(result[1].decode('utf-8'))
+            middleSet.add(result[1].decode('utf-8'))
+            #print("Name: " + str(result[0].decode("utf-8")))
+            result = curs.next()
     else:
         print("No result was found")
         return ()
-            
+
+    curs.close()
+    database.close()
     return middleSet
 
 
-
-
 def main():
-    print(yearSearch(1977))
+    yearSearch(2017)
 
 
 if __name__ == "__main__":
